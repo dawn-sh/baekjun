98fb5b696bf451899c954a3d4db6e58c11b4c618
sunghyun <tjdgus9773@gmail.com>
2024-01-08 21:05:50 +0900

    baekjun study 10828

---
+++ baekjoon/src/baekjoon/B11005.java
@@ -0,0 +1,23 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11005 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		
+		int N=Integer.parseInt(st.nextToken());
+		int B=Integer.parseInt(st.nextToken());
+		
+		
+		
+	}
+
+}
